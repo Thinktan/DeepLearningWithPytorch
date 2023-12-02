0004cf04b5c32641af017ed611a9016082edbe0c
Thinktan <tantao0675@hnu.edu.cn>
2023-12-02 11:17:57 +0800

    up

--- p1ch5/1_parameter_estimation.py
+++ p1ch5/1_parameter_estimation.py
@@ -26,4 +26,57 @@ loss = loss_fn(t_p, t_c)
 print(loss)
 
 
+def dloss_fn(t_p, t_c):
+    dsq_diffs = 2 * (t_p - t_c) / t_p.size(0)
+    return dsq_diffs
 
+def dmodel_dw(t_u, w, b):
+    return t_u
+
+def dmodel_db(t_u, w, b):
+    return 1.0
+
+def grad_fn(t_u, t_c, t_p, w, b):
+    dloss_dtp = dloss_fn(t_p, t_c)
+    dloss_dw = dloss_dtp * dmodel_dw(t_u, w, b)
+    dloss_db = dloss_dtp * dmodel_db(t_u, w, b)
+    return torch.stack([dloss_dw.sum(), dloss_db.sum()])
+
+def training_loop(n_epochs, learning_rate, params, t_u, t_c):
+    for epoch in range(1, n_epochs+1):
+        w, b = params
+
+        t_p = model(t_u, w, b)
+        loss = loss_fn(t_p, t_c)
+        grad = grad_fn(t_u, t_c, t_p, w, b)
+
+        params = params - learning_rate*grad
+
+        print('Epoch %d, Loss %f' % (epoch, float(loss)))
+
+    return params
+
+t_un = 0.1*t_u
+
+params = training_loop(n_epochs = 5000,
+              learning_rate = 1e-2,
+              params = torch.tensor([1.0, 0.0]),
+              t_u = t_un, t_c = t_c)
+
+
+print("params: ", params)
+
+# 绘制数据
+from matplotlib import pyplot as plt
+
+t_p = model(t_un, *params)
+
+fig = plt.figure(dpi=600)
+
+plt.xlabel("Temperature (F)")
+plt.ylabel("Temperature (C)")
+
+plt.plot(t_u.numpy(), t_p.detach().numpy())
+plt.plot(t_u.numpy(), t_c.numpy(), 'o')
+
+plt.show()
