0705dc704779d7c4caf33e28180c3b1e9127d7c7
Thinktan <tantao0675@hnu.edu.cn>
2023-12-05 01:11:29 +0800

    up w

--- p1ch7/2_birds_airplanes.py
+++ p1ch7/2_birds_airplanes.py
@@ -1,9 +1,6 @@
-from matplotlib import pyplot as plt
-import numpy as np
-import torch
-import torch.nn as nn
-import torch.optim as optim
 
+import torch
+import torch.nn.functional as F
 torch.set_printoptions(edgeitems=2)
 torch.manual_seed(123)
 
@@ -170,6 +167,56 @@ numel_list = [p.numel() for p in model.parameters()]
 print(sum(numel_list), numel_list)
 
 
+# 8.3 子类化nn.Module
+class Net1(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.act1 = nn.Tanh()
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
+        self.act2 = nn.Tanh()
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(8 * 8 * 8, 32)
+        self.act3 = nn.Tanh()
+        self.fc2 = nn.Linear(32, 2)
+
+    def forward(self, x):
+        out = self.pool1(self.act1(self.conv1(x)))
+        out = self.pool2(self.act2(self.conv2(out)))
+        out = out.view(-1, 8 * 8 * 8)
+        out = self.act3(self.fc1(out))
+        out = self.fc2(out)
+        return out
+
+model = Net1()
+
+numel_list = [p.numel() for p in model.parameters()]
+print(sum(numel_list), numel_list)
+
+# 8.3.3 函数式api
+
+
+class Net2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 8, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(8 * 8 * 8, 32)
+        self.fc2 = nn.Linear(32, 2)
+
+    def forward(self, x):
+        out = F.max_pool2d(torch.tanh(self.conv1(x)), 2)
+        out = F.max_pool2d(torch.tanh(self.conv2(out)), 2)
+        out = out.view(-1, 8 * 8 * 8)
+        out = torch.tanh(self.fc1(out))
+        out = self.fc2(out)
+        return out
+
+
+# In[29]:
+model = Net2()
+print(model(img.unsqueeze(0)))
 
 
 
