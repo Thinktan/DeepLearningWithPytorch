23472e721e4b1e30adcfe9f476d7939b381e0bb9
Thinktan <tantao0675@hnu.edu.cn>
2023-12-10 00:34:16 +0800

    up w

--- p2ch10/dsets.py
+++ p2ch10/dsets.py
@@ -5,6 +5,20 @@ import csv
 import os
 import numpy as np
 import SimpleITK as sitk
+from util.util import XyzTuple, xyz2irc
+from torch.utils.data import Dataset
+import torch
+from util.disk import getCache
+
+from util.logconf import logging
+
+log = logging.getLogger(__name__)
+# log.setLevel(logging.WARN)
+# log.setLevel(logging.INFO)
+log.setLevel(logging.DEBUG)
+
+raw_cache = getCache('part2ch10_raw')
+
 
 CandidateInfoTuple = namedtuple(
   'CandidateInfoTuple',
@@ -85,7 +99,125 @@ class Ct:
         ct_a.clip(-1000, 1000, ct_a)
 
         self.series_uid = series_uid
-        self.hu_a = ct_a
+        self.hu_a = ct_a  # irc坐标相关信息
+
+        # 转换成irc规格所需要的全部信息
+        self.origin_xyz = XyzTuple(*ct_mhd.GetOrigin())
+        self.vxSize_xyz = XyzTuple(*ct_mhd.GetSpacing())
+        self.direction_a = np.array(ct_mhd.GetDirection()).reshape(3, 3)
+
+    def getRawCandidate(self, center_xyz, width_irc):
+        center_irc = xyz2irc(
+            center_xyz,
+            self.origin_xyz,
+            self.vxSize_xyz,
+            self.direction_a,
+        )
+
+        slice_list = []
+        for axis, center_val in enumerate(center_irc):
+            start_ndx = int(round(center_val - width_irc[axis] / 2))
+            end_ndx = int(start_ndx + width_irc[axis])
+
+            if start_ndx < 0:
+                log.warning("Crop outside of CT array: {} {}, center:{} shape:{} width:{}".format(
+                    self.series_uid, center_xyz, center_irc, self.hu_a.shape, width_irc))
+                start_ndx = 0
+                end_ndx = int(width_irc[axis])
+
+            if end_ndx > self.hu_a.shape[axis]:
+                log.warning("Crop outside of CT array: {} {}, center:{} shape:{} width:{}".format(
+                    self.series_uid, center_xyz, center_irc, self.hu_a.shape, width_irc))
+                end_ndx = self.hu_a.shape[axis]
+                start_ndx = int(self.hu_a.shape[axis] - width_irc[axis])
+
+            slice_list.append(slice(start_ndx, end_ndx))
+
+        ct_chunk = self.hu_a[tuple(slice_list)]
+
+        return ct_chunk, center_irc
+
+
+@functools.lru_cache(1, typed=True)
+def getCt(series_uid):
+    return Ct(series_uid)
+
+@raw_cache.memoize(typed=True)
+def getCtRawCandidate(series_uid, center_xyz, width_irc):
+    ct = getCt(series_uid)
+    ct_chunk, center_irc = ct.getRawCandidate(center_xyz, width_irc)
+    return ct_chunk, center_irc
+
+class LunaDataset(Dataset):
+    def __init__(self,
+                 val_stride=0,
+                 isValSet_bool=None,
+                 series_uid=None,
+                 ):
+        '''
+
+        :param val_stride:
+        :param isValSet_bool:
+        :param series_uid:传入一个真实的series_uid，实例将只包含来自该序列的结节。对可视化或调试是有用
+        '''
+        self.candidateInfo_list = copy.copy(getCandidateInfoList())
+
+        if series_uid:
+            self.candidateInfo_list = [
+                x for x in self.candidateInfo_list if x.series_uid == series_uid
+            ]
+
+        if isValSet_bool:
+            assert val_stride > 0, val_stride
+            self.candidateInfo_list = self.candidateInfo_list[::val_stride]
+            assert self.candidateInfo_list
+        elif val_stride > 0:
+            del self.candidateInfo_list[::val_stride]
+            assert self.candidateInfo_list
+
+        log.info("{!r}: {} {} samples".format(
+            self,
+            len(self.candidateInfo_list),
+            "validation" if isValSet_bool else "training",
+        ))
+
+    def __len__(self):
+        return len(self.candidateInfo_list)
+
+    def __getitem__(self, ndx):
+        candidateInfo_tup = self.candidateInfo_list[ndx]
+        width_irc = (32, 48, 48)
+
+        candidate_a, center_irc = getCtRawCandidate(
+            candidateInfo_tup.series_uid,
+            candidateInfo_tup.center_xyz,
+            width_irc,
+        )
+
+        candidate_t = torch.from_numpy(candidate_a)
+        candidate_t = candidate_t.to(torch.float32)
+        candidate_t = candidate_t.unsqueeze(0)
+
+        pos_t = torch.tensor([
+            not candidateInfo_tup.isNodule_bool,
+            candidateInfo_tup.isNodule_bool
+        ],
+            dtype=torch.long,
+        )
+
+        return (
+            candidate_t,
+            pos_t,
+            candidateInfo_tup.series_uid,
+            torch.tensor(center_irc),
+        )
+
+
+
+
+
+
+
 
 
 
--- util/util.py
+++ util/util.py
@@ -16,3 +16,50 @@ log.setLevel(logging.DEBUG)
 IrcTuple = collections.namedtuple('IrcTuple', ['index', 'row', 'col'])
 XyzTuple = collections.namedtuple('XyzTuple', ['x', 'y', 'z'])
 
+'''
+xyz -> cri
+x -> column
+y -> row
+z -> index
+'''
+
+def irc2xyz(coord_irc, origin_xyz, vxSize_xyz, direction_a):
+    '''
+
+    :param coord_irc: 要转换的IRC坐标
+    :param origin_xyz: XYZ坐标系中的原点
+    :param vxSize_xyz: 体素大小
+    :param direction_a: 方向矩阵，用于处理影像方向
+    :return:
+    '''
+    cri_a = np.array(coord_irc)[::-1] # 转numpy，反转顺序
+
+    origin_a = np.array(origin_xyz) # 转numpy
+    vxSize_a = np.array(vxSize_xyz)
+
+    # cri_a * vxSize_a 首先将IRC坐标乘以体素大小，将其转换为物理空间尺寸。
+    # 然后通过矩阵 direction_a 乘以这个结果，来考虑影像的方向。最后加上原点 origin_a。
+    coords_xyz = (direction_a @ (cri_a * vxSize_a)) + origin_a
+    # coords_xyz = (direction_a @ (idx * vxSize_a)) + origin_a
+    return XyzTuple(*coords_xyz)
+
+def xyz2irc(coord_xyz, origin_xyz, vxSize_xyz, direction_a):
+    '''
+
+    :param coord_xyz: 要转换的XYZ坐标
+    :param origin_xyz: XYZ坐标系中的原点
+    :param vxSize_xyz: 体素大小
+    :param direction_a: 方向矩阵，用于考虑影像的方向
+    :return:
+    '''
+    origin_a = np.array(origin_xyz)
+    vxSize_a = np.array(vxSize_xyz)
+    coord_a = np.array(coord_xyz)
+
+    # (coord_a - origin_a) 将 XYZ 坐标相对于原点进行调整。
+    # 然后，使用 @（矩阵乘法）将这个调整后的坐标与方向矩阵的逆矩阵 np.linalg.inv(direction_a) 相乘。
+    # 这一步是必要的，因为影像的方向可能与标准的 XYZ 坐标系不同。
+    # 最后，将结果除以体素大小 vxSize_a，将物理尺寸转换回 IRC 坐标。
+    cri_a = ((coord_a - origin_a) @ np.linalg.inv(direction_a)) / vxSize_a
+    cri_a = np.round(cri_a)
+    return IrcTuple(int(cri_a[2]), int(cri_a[1]), int(cri_a[0]))
